04ccffce838df73dc44e22603645271f5efb97eb
Christopher David <chris@arcade.city>
2022-10-12 12:10:52 -0400

    Screen - n oheader

--- packages/app/navigation/native/index.tsx
+++ packages/app/navigation/native/index.tsx
@@ -14,6 +14,7 @@ export function NativeNavigation() {
         name="map"
         component={MapScreen}
         options={{
+          headerShown: false,
           title: 'Map',
         }}
       />
