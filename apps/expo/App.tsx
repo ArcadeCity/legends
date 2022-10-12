4e2acac4a028f3a9ffc7c2f70d043cb95fa7cf8f
Christopher David <chris@arcade.city>
2022-10-12 14:41:21 -0400

    Add useExpoUpdates and supporting hooks

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 import { useFonts } from 'expo-font'
+import { useExpoUpdates } from './lib/useExpoUpdates'
 
 export default function App() {
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   })
+
+  useExpoUpdates(3)
 
   if (!loaded) {
     return null
