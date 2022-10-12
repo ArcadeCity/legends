b5aa9e911e2c5292e3f775f11c2c3d307a6b0a49
Christopher David <chris@arcade.city>
2022-10-12 10:22:22 -0400

    Add Fathom code to _app.tsx

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -2,19 +2,41 @@ import '@tamagui/core/reset.css'
 import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
 
+import * as Fathom from 'fathom-client'
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
 import { Provider } from 'app/provider'
 import Head from 'next/head'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import type { SolitoAppProps } from 'solito'
 import 'raf/polyfill'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   const [theme, setTheme] = useRootTheme()
+  const router = useRouter()
 
   const contents = useMemo(() => {
     return <Component {...pageProps} />
   }, [pageProps])
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('HRLSLWOY', {
+      includedDomains: ['legends.arcade.city'],
+      url: 'https://abacus.arcade.city/script.js',
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
 
   return (
     <>
