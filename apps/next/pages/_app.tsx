d17e2e09799e98ccc94b7f9b524f43773804da76
Christopher David <chris@arcade.city>
2022-10-12 09:28:18 -0500

    Add Fathom analytics (#3)
    * next: yarn add -E fathom-client
    
    * Add Fathom code to _app.tsx
    
    * Page title / favicon
    
    * Use correct Fathom project id

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -2,25 +2,47 @@ import '@tamagui/core/reset.css'
 import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
 
+import * as Fathom from 'fathom-client'
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
 import { Provider } from 'app/provider'
 import Head from 'next/head'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import type { SolitoAppProps } from 'solito'
 import 'raf/polyfill'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   const [theme, setTheme] = useRootTheme()
+  const router = useRouter()
 
   const contents = useMemo(() => {
     return <Component {...pageProps} />
   }, [pageProps])
 
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('ILZBOUGJ', {
+      includedDomains: ['legends.arcade.city'],
+      url: 'https://abacus.arcade.city/script.js',
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
-        <title>Tamagui Example App</title>
-        <meta name="description" content="Tamagui, Solito, Expo & Next.js" />
+        <title>Arcade @ Legends</title>
+        <meta name="description" content="Placeholder!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NextThemeProvider onChangeTheme={setTheme}>
