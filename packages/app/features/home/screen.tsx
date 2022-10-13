7309584b922ce59971937ebd5a3c942758546afa
Christopher David <chris@arcade.city>
2022-10-13 15:10:47 -0400

    Style it

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -46,26 +46,46 @@ export function SheetDemo() {
   return (
     <>
       <Button
-        size="$6"
+        size="$7"
         icon={open ? ChevronDown : ChevronUp}
         circular
+        color="white"
+        backgroundColor="rgba(0, 248, 248, 0.4)"
+        hoverStyle={{
+          backgroundColor: 'rgba(0, 248, 248, 0.6)',
+          borderColor: 'rgba(0, 248, 248, 1)',
+        }}
+        borderColor="rgba(0, 248, 248, 0.8)"
+        borderWidth={1}
         onPress={() => setOpen((x) => !x)}
       />
       <Sheet
         modal
         open={open}
         onOpenChange={setOpen}
-        snapPoints={[80]}
+        snapPoints={[90]}
         position={position}
         onPositionChange={setPosition}
         dismissOnSnapToBottom
       >
         <Sheet.Overlay />
-        <Sheet.Frame ai="center" jc="center">
+        <Sheet.Frame
+          ai="center"
+          jc="center"
+          backgroundColor="#120B29"
+          borderTopColor="#46367C"
+          borderWidth={1}
+        >
           <Sheet.Handle />
           <Button
-            size="$6"
+            size="$7"
             circular
+            color="white"
+            backgroundColor="rgba(0, 248, 248, 0.4)"
+            hoverStyle={{
+              backgroundColor: 'rgba(0, 248, 248, 0.6)',
+              borderColor: 'rgba(0, 248, 248, 1)',
+            }}
             icon={ChevronDown}
             onPress={() => {
               setOpen(false)
