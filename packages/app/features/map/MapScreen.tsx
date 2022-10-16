818149a9c7d0dcf54e83a2920341e03b0477e92e
Christopher David <chris@arcade.city>
2022-10-16 11:00:31 -0500

    connect to sockjs from expo

--- packages/app/features/map/MapScreen.tsx
+++ packages/app/features/map/MapScreen.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { WebView } from 'react-native-webview'
+import SockJS from 'sockjs-client'
 import { Hud } from '../hud/Hud'
 
 export function MapScreen() {
+  useEffect(() => {
+    var sock = new SockJS('https://realgame-dev.arcade.city/echo')
+    sock.onopen = function () {
+      console.log('open')
+      sock.send('test')
+    }
+
+    sock.onmessage = function (e) {
+      console.log('message', e.data)
+      sock.close()
+    }
+
+    sock.onclose = function () {
+      console.log('close')
+    }
+  }, [])
   return (
     <>
       <WebView style={styles.container} source={{ uri: 'https://vmap.arcade.city' }} />
