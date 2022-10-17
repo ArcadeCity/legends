ba988de6f2ff6bcd0c01f645c51c6fad3d98f6e0
Christopher David <chris@arcade.city>
2022-10-17 08:05:47 -0500

    swap in rapier physics example

--- packages/app/features/multiplayer/MultiplayerDemo.tsx
+++ packages/app/features/multiplayer/MultiplayerDemo.tsx
@@ -1,17 +1,26 @@
-import { Physics } from '@react-three/cannon'
-import { OrbitControls } from '@react-three/drei'
+import { Suspense } from 'react'
+import { Box, OrbitControls, Torus } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { Physics, RigidBody } from '@react-three/rapier'
 import { Player } from './Player'
 
 export const MultiplayerDemo = () => {
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10, 10, 10]} /> */}
 
-      <Physics>
-        <Player />
-      </Physics>
+      <Suspense fallback={null}>
+        <Physics>
+          <RigidBody colliders={'hull'} restitution={2}>
+            <Torus />
+          </RigidBody>
+
+          <RigidBody position={[0, -2, 0]} type="kinematicPosition">
+            <Box args={[20, 0.5, 20]} />
+          </RigidBody>
+        </Physics>
+      </Suspense>
 
       <OrbitControls />
     </Canvas>
