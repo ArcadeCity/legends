2ec9f502d91de42b3f8b966b2db0ca83021f3d7f
Christopher David <chris@arcade.city>
2022-10-17 08:56:23 -0500

    test initial WASD controls

--- packages/app/features/multiplayer/Player.tsx
+++ packages/app/features/multiplayer/Player.tsx
@@ -1,9 +1,26 @@
+import { useRef } from 'react'
 import { Box } from '@react-three/drei'
-import { RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+import { RigidBody, RigidBodyApi } from '@react-three/rapier'
+import { usePersonControls } from './usePersonControls'
 
 export const Player = () => {
+  const player = useRef<RigidBodyApi>(null)
+  const { forward, backward, left, right, jump } = usePersonControls()
+  const torque = 0.1
+  useFrame(() => {
+    if (player.current) {
+      player.current.applyTorqueImpulse({
+        x: (right ? torque : 0) + (left ? -torque : 0),
+        y: 0,
+        // y: player.current.getLinearVelocity().y,
+        z: (forward ? torque : 0) + (backward ? -torque : 0),
+      })
+      if (jump) player.current.applyTorqueImpulse({ x: 0, y: 5, z: 0 })
+    }
+  })
   return (
-    <RigidBody position={[0, 4, 0]} colliders={'hull'} restitution={0.3}>
+    <RigidBody ref={player} position={[0, 4, 0]} colliders={'hull'} restitution={0.3}>
       <Box castShadow>
         <meshStandardMaterial color="darkorange" />
       </Box>
