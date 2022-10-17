e6501a2199f9e600852ed69ba23d8b3cf1d72129
Christopher David <chris@arcade.city>
2022-10-17 08:08:41 -0500

    add color materials to shapes

--- packages/app/features/multiplayer/MultiplayerDemo.tsx
+++ packages/app/features/multiplayer/MultiplayerDemo.tsx
@@ -7,17 +7,28 @@ import { Player } from './Player'
 export const MultiplayerDemo = () => {
   return (
     <Canvas>
-      {/* <ambientLight /> */}
-      {/* <pointLight position={[10, 10, 10]} /> */}
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
 
       <Suspense fallback={null}>
         <Physics>
           <RigidBody colliders={'hull'} restitution={2}>
-            <Torus />
+            <Torus>
+              <meshStandardMaterial color="blue" />
+            </Torus>
+          </RigidBody>
+
+          <RigidBody colliders={'hull'} restitution={2}>
+            <Torus args={[1, 0.5, 16, 100]}>
+              <meshStandardMaterial color="orange" />
+            </Torus>
           </RigidBody>
 
           <RigidBody position={[0, -2, 0]} type="kinematicPosition">
-            <Box args={[20, 0.5, 20]} />
+            {/* Add a green material */}
+            <Box args={[20, 0.5, 20]}>
+              <meshStandardMaterial color="green" />
+            </Box>
           </RigidBody>
         </Physics>
       </Suspense>
